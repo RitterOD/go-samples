1ea0f1af407d1d4ea839f207938a2a4561c2deda
Alexander Maslov <ritterod@gmail.com>
2023-03-19 18:11:21 +0300

    go-samples: initial stuff for graph alg examples
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ alg/model/graph.go
@@ -0,0 +1,3 @@
+package model
+
+type Graph map[int][]int
---
+++ alg/model/graph_test.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"testing"
+)
+
+func testGraphCreation(t *testing.T) {
+
+	g := make(Graph)
+	g[0] = []int{1, 2}
+	g[1] = []int{0, 3}
+	g[2] = []int{0, 3, 4}
+	g[3] = []int{1, 2}
+	g[4] = []int{2}
+}
