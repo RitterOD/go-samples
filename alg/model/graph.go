1b87a428e78b016a8f44ca4940dc50515913a825
Alexander Maslov <ritterod@gmail.com>
2023-03-21 19:46:25 +0300

    go-samples: impovements of graph type
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- alg/model/graph.go
+++ alg/model/graph.go
@@ -1,3 +1,45 @@
 package model
 
-type Graph map[int][]int
+type GraphRep map[int][]int
+
+type GraphType int64
+
+const (
+	DIRECTED   GraphType = 0
+	UNDIRECTED GraphType = 1
+)
+
+type Graph struct {
+	rep          GraphRep
+	vertexToName map[int]string
+	nameToVertex map[string]int
+	graphType    GraphType
+}
+
+func NewGraph(graphType GraphType) *Graph {
+	g := new(Graph)
+	g.rep = make(GraphRep)
+	g.nameToVertex = make(map[string]int)
+	g.vertexToName = make(map[int]string)
+	return g
+}
+
+// TODO ERROR CHECK
+
+func (g Graph) AddVertex(v int, name string) {
+	g.rep[v] = []int{}
+	g.nameToVertex[name] = v
+	g.vertexToName[v] = name
+}
+
+// TODO ERROR CHECK
+func (g Graph) AddEdge(u, v int) {
+	if g.graphType == DIRECTED {
+		g.rep[v] = append(g.rep[v], u)
+	} else if g.graphType == UNDIRECTED {
+		g.rep[v] = append(g.rep[v], u)
+		g.rep[u] = append(g.rep[u], v)
+	} else {
+		panic("Not supported graph type")
+	}
+}
--- alg/model/graph_test.go
+++ alg/model/graph_test.go
@@ -6,7 +6,7 @@ import (
 
 func testGraphCreation(t *testing.T) {
 
-	g := make(Graph)
+	g := make(GraphRep)
 	g[0] = []int{1, 2}
 	g[1] = []int{0, 3}
 	g[2] = []int{0, 3, 4}
