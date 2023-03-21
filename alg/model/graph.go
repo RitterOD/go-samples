6fa2b81075d60b84c4fc93dd5997f925dd595644
Alexander Maslov <ritterod@gmail.com>
2023-03-21 20:59:06 +0300

    go-samples: add dot graph representation for graph
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- alg/model/graph.go
+++ alg/model/graph.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"strings"
+)
+
 type GraphRep map[int][]int
 
 type GraphType int64
@@ -19,6 +23,7 @@ type Graph struct {
 func NewGraph(graphType GraphType) *Graph {
 	g := new(Graph)
 	g.rep = make(GraphRep)
+	g.graphType = graphType
 	g.nameToVertex = make(map[string]int)
 	g.vertexToName = make(map[int]string)
 	return g
@@ -35,11 +40,30 @@ func (g Graph) AddVertex(v int, name string) {
 // TODO ERROR CHECK
 func (g Graph) AddEdge(u, v int) {
 	if g.graphType == DIRECTED {
-		g.rep[v] = append(g.rep[v], u)
+		g.rep[u] = append(g.rep[u], v)
 	} else if g.graphType == UNDIRECTED {
 		g.rep[v] = append(g.rep[v], u)
 		g.rep[u] = append(g.rep[u], v)
 	} else {
 		panic("Not supported graph type")
 	}
+}
+
+func (g Graph) getDotRepresentation() string {
+	nodes := make([]string, 0)
+	for n, _ := range g.nameToVertex {
+		nodes = append(nodes, n)
+	}
+	edges := make([]string, 0)
+	if g.graphType == DIRECTED {
+		for v, adjNodes := range g.rep {
+			for _, u := range adjNodes {
+				edges = append(edges, g.vertexToName[v]+" -> "+g.vertexToName[u])
+			}
+		}
+		return "digraph { " + strings.Join(nodes[:], ";\n") + "\n" + strings.Join(edges[:], "\n") + " }"
+	} else {
+		panic("UNSUPPORTED GRAPH TYPE")
+	}
+
 }
--- alg/model/graph_test.go
+++ alg/model/graph_test.go
@@ -1,10 +1,11 @@
 package model
 
 import (
+	"fmt"
 	"testing"
 )
 
-func testGraphCreation(t *testing.T) {
+func TestGraphCreation(t *testing.T) {
 
 	g := make(GraphRep)
 	g[0] = []int{1, 2}
@@ -12,4 +13,22 @@ func testGraphCreation(t *testing.T) {
 	g[2] = []int{0, 3, 4}
 	g[3] = []int{1, 2}
 	g[4] = []int{2}
+}
+
+func TestGetDotGraphStringRepresentation(t *testing.T) {
+	g := NewGraph(DIRECTED)
+	g.AddVertex(0, "A")
+	g.AddVertex(1, "B")
+	g.AddVertex(2, "C")
+	g.AddVertex(3, "D")
+	g.AddVertex(4, "E")
+
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+	g.AddEdge(1, 3)
+	g.AddEdge(2, 4)
+	g.AddEdge(3, 2)
+
+	dotRep := g.getDotRepresentation()
+	fmt.Println(dotRep)
 }
