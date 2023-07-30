f4dc77e6a04f33dddc1cfcb65c2ab34c18b20afd
Alexander Maslov <ritterod@gmail.com>
2023-07-30 11:44:13 +0300

    #6 go-samples: imporve graphgenerator
    1)add intercluster edge generation
    2)add writing result to file
    
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- alg/graphgenerator/graphgenerator.go
+++ alg/graphgenerator/graphgenerator.go
@@ -1,7 +1,6 @@
 package graphgenerator
 
 import (
-	"fmt"
 	"math"
 	"math/rand"
 	"sample/alg/model"
@@ -12,13 +11,14 @@ import (
 func ClusteredRandomGraph(minClusterSize, maxClusterSize int,
 	minClusterCount, maxClusterCount int,
 	clusterEdgeProbability, interClusterEdgeProbability float64,
-	minWeight, maxWeight float64) *model.WeightedGraph {
+	minWeight, maxWeight float64, interClusterMinWeight, interClusterMaxWeight float64) *model.WeightedGraph {
 	rv := model.NewWeightedGraph()
 	randSource := rand.NewSource(time.Now().UnixNano())
 	randGenerator := rand.New(randSource)
 	clusterCount := minClusterCount + int(math.Round(randGenerator.Float64()*float64(maxClusterCount-minClusterCount)))
 	currentClusterMinVertexIndex := 0
 	currentClusterMaxVertexIndex := 0
+	clusterIdToVertexRange := make(map[int][]int)
 	for i := 0; i < clusterCount; i++ {
 		clusterSize := minClusterSize + int(math.Round(randGenerator.Float64()*float64(maxClusterSize-minClusterSize)))
 		for j := 0; j < clusterSize; j++ {
@@ -29,17 +29,31 @@ func ClusteredRandomGraph(minClusterSize, maxClusterSize int,
 			for k := currentClusterMinVertexIndex; k < currentClusterMaxVertexIndex; k++ {
 				if j != k {
 					currentProb := rand.Float64()
-					addEdge := currentProb > clusterEdgeProbability
-					fmt.Println("currentProbability:" + fmt.Sprint(clusterSize))
+					addEdge := currentProb < clusterEdgeProbability
+					//fmt.Println("currentProbability:" + fmt.Sprint(clusterSize))
 					if addEdge {
 						rv.AddEdge(j, k, minWeight+rand.Float64()*(maxWeight-minWeight))
-						fmt.Println("edge added : " + fmt.Sprint(j) + " -> " + fmt.Sprint(k))
+						//fmt.Println("edge added : " + fmt.Sprint(j) + " -> " + fmt.Sprint(k))
 					}
 				}
 			}
 		}
+		clusterIdToVertexRange[i] = []int{currentClusterMinVertexIndex, currentClusterMaxVertexIndex}
 		currentClusterMinVertexIndex += clusterSize
 
+	}
+	for _, r := range clusterIdToVertexRange {
+		for _, rTo := range clusterIdToVertexRange {
+			if r[0] != rTo[0] {
+				currentProb := rand.Float64()
+				if currentProb < interClusterEdgeProbability {
+					outVertexInd := r[0] + int(math.Round(float64(r[1]-r[0])*rand.Float64()))
+					inVertexInd := rTo[0] + int(math.Round(float64(rTo[1]-rTo[0])*rand.Float64()))
+					rv.AddEdge(outVertexInd, inVertexInd, interClusterMinWeight+
+						rand.Float64()*(interClusterMaxWeight-interClusterMinWeight))
+				}
+			}
+		}
 	}
 	return rv
 }
--- alg/graphgenerator/graphgenerator_test.go
+++ alg/graphgenerator/graphgenerator_test.go
@@ -2,12 +2,35 @@ package graphgenerator
 
 import (
 	"fmt"
+	"os"
 	"testing"
+	"time"
 )
 
 func TestGenerator(t *testing.T) {
-	//t.Error("TEST_ERROR")
-	graph := ClusteredRandomGraph(5, 5, 1, 1, 0.5, 0.5, 1, 100)
+	graph := ClusteredRandomGraph(5, 5, 1, 1, 0.5, 0.5, 1, 100, 1000, 1500)
 	fmt.Println(graph.GetDotRepresentation())
+}
+
+func TestGeneratorTwoClusters(t *testing.T) {
+	graph := ClusteredRandomGraph(5, 5, 2, 2, 0.5, 1, 1, 100, 1000, 1500)
+	fmt.Println(graph.GetDotRepresentation())
+}
+
+func TestGenerateToFile(t *testing.T) {
+	graph := ClusteredRandomGraph(100, 100, 1, 1, 0.5, 1, 1, 100, 1000, 1500)
+	//fmt.Println(graph.GetDotRepresentation())
+	fileName := "oneCluster_" + time.Now().String() + ".dot"
+	f, err := os.Create(fileName)
+
+	if err != nil {
+		t.Error("Can't create file: " + fileName)
+	} else {
+		_, err := f.WriteString(graph.GetDotRepresentation())
+		if err != nil {
+			t.Error("Can't write to  file: " + fileName)
+		}
+	}
+	defer f.Close()
 
 }
